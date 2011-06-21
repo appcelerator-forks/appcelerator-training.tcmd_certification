77f3d220900bb3434e36a8680fca02e8471a0f44
Infinitation <conrad@threesphere.com>
2011-06-21 12:16:38 -0500

    Final code for 310 lab

--- 310/code/Resources/app.js
+++ 310/code/Resources/app.js
@@ -19,27 +19,89 @@ var ct = {};
 			image: 'images/tabBkg.png',
 			width: 'auto',
 			height: 'auto', 
-			left: -323
+			left: -320
 		})
 		win.add(bkgImg);
 
+		/*
+		This function controls the make of the tabs. This function does
+		all the heavy lifting of the application
+		*/
 		var makeTab = function(/*Object*/ obj) {
 			var tt = Ti.UI.createTab({
 				title: obj.title,
 				window: obj.window
 			})
 			tg.addTab(tt);
+			
+			var tview = Ti.UI.createView({
+				width: 100,
+				height: 45,
+				bottom: 2,
+				left: obj.loc
+			})
+			
+			tview.addEventListener('click', function(e) {
+				animateCt(obj.animate);
+				tg.setActiveTab(obj.active);
+			});
+			
+			
+			var img = Ti.UI.createImageView({
+				image: "images/"+obj.img,
+				width: "auto",
+				height: "auto",
+				top: 0
+			});
+			tview.add(img);
+	
+			var lbl = Ti.UI.createLabel({
+				text: obj.title,
+				font: {fontWeight: 'bold', fontSize: 10},
+				color: '#fff',
+				width: 'auto',
+				height: 'auto',
+				bottom: 0,
+				shadowColor: '#000',
+				shadowOffset: {x:0, y:1}
+			});
+			tview.add(lbl);
+			
+			win.add(tview);
+			
 		}
 		
+		/*
+		The simple animation function to move the background image of the tab
+		*/
+		var animateCt = function(loc) {
+			var a = Ti.UI.createAnimation();
+				a.left = loc;
+				a.duration = 500;
+			bkgImg.animate(a);
+		}
+		
+		
+		/*
+		This is the simple object to create the tabs. A great way to improve this is to auctomate
+		the loc, and animate, by using the built in functions to determine platform width and
+		quantity of items you want.
+		*/
 		makeTab({
-			loc: 50,
-			title: 'today',
+			loc: 20,
+			animate: -320,
+			title: 'facebook',
+			img: 'Facebook.png',
+			active: 0,
 			window: ct.ui.createMainWindow()
 		});
 		
 		makeTab({
-			loc: 50,
-			title: 'another',
+			loc: 200,
+			animate: -140,
+			title: 'twitter',
+			img: 'Twitter.png',
+			active: 1,
 			window: ct.ui.createSecondaryWindow()
 		});
 		
@@ -48,9 +110,15 @@ var ct = {};
 		return win;
 	}
 	
+	/*
+	This is the main window that is intially launched, these windows can 
+	be externalized for better coding composition, for this demo
+	there are here for ease
+	*/
 	ct.ui.createMainWindow = function() {
 		var win = Ti.UI.createWindow({
-			backgroundColor: '#ddd'
+			backgroundColor: '#ddd',
+			title: 'Facebook'
 		})
 		
 		return win;
@@ -59,7 +127,8 @@ var ct = {};
 	
 	ct.ui.createSecondaryWindow = function() {
 		var win = Ti.UI.createWindow({
-			backgroundColor: '#ccc'
+			backgroundColor: '#ccc',
+			title: 'Twitter'
 		})
 		
 		return win;
