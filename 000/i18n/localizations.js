9c9154f7bbcd79f3ca50785d6544ad39210a4831
Tim Poulsen <tim@skypanther.com>
2012-04-13 11:54:25 -0400

    Lesson 000 and 300 updated, CSS fixes

--- 000/i18n/localizations.js
+++ 000/i18n/localizations.js
@@ -7,18 +7,21 @@ String.toLocaleString({
 	"en-US": {
 		"%l_filetitle": "Advanced Titanium Mobile Development",
 		"%l_lessontitle": "Advanced Titanium Mobile Development",
-		"%l_slidenote_titleslide": "<b>Module time: 60 minutes</b> (30 mins teaching, 30 mins lab)",
+		"%l_slidenote_titleslide": "",
+
 		"%l_logisitics": "Logistics",
 		"%l_logisitics1": "Daily Schedule",
 		"%l_logisitics2": "Facilities Review",
 		"%l_logisitics3": "Disturbances (phones, etc.)",
-		"%l_slidenote_logisitics": "Give the daily schedule: start/stop times, etc.<br>Review facilities – restrooms, break and lunch rooms, etc.<br>Phones on vibrate, respect the silence and work needs of the others around you",
+		"%l_slidenote_logisitics": "Give the daily schedule: start/stop times, etc.<br>Review facilities &mdash; restrooms, break and lunch rooms, etc.<br>Phones on vibrate, respect the silence and work needs of the others around you",
+
 		"%l_rationale": "Rationale",
 		"%l_adv_training": "'Advanced' training could mean any number of things",
 		"%l_basis": "Basis for TCMD certification",
 		"%l_we_define": "We define advanced training as the following:",
 		"%l_exposing": "Exposing developers to the tools, APIs, and platform-specific configurations to deliver 'best of breed' native mobile applications",
-		"%l_slidenote_rationale": "'Advanced' is a loaded term – what’s advanced to you might not be what’s advanced to me, so we do the best we can to define what we mean by advanced<br>As a practical matter, our advanced training is the basis for TCMD<br>'Best of breed' means delivering the best of the target platform – deep OS integration, solid performance, and polished user interface",
+		"%l_slidenote_rationale": "'Advanced' is a loaded term &mdash; what's advanced to you might not be what's advanced to me, so we do the best we can to define what we mean by advanced<br>As a practical matter, our advanced training is the basis for TCMD<br>'Best of breed' means delivering the best of the target platform &mdash; deep OS integration, solid performance, and polished user interface",
+
 		"%l_roadmap": "Training Roadmap",
 		"%l_dayone": "Day One",
 		"%l_daytwo": "Day Two",
@@ -35,9 +38,11 @@ String.toLocaleString({
 		"%l_input": "Input Collection",
 		"%l_office": "Office Hours",
 		"%l_scrollview": "ScrollView API",
-		"%l_slidenote_roadmap": "We’ll start by sharing techniques we’ve learned for creating performant apps<br>Good UX separates average from best of breed apps<br>Advanced UI digs into creating custom UI components<br>TableView – dig deep into creating custom tables and rows<br>Gestures – handle orientation, shakes, and other gestures<br>Input – mobile devices not optimal for data entry, so we’ll show some tricks and techniques<br>ScrollView – ScrollView and ScrollableView components, expose functionality over time rather than jamming all your UI into the viewport<br>Animation – important from a UX standpoint, can add eye-candy to your app also<br>Android API – dig into some android specific features you can implement<br>iOS API – dig into iOS specific UI components, add to settings screen<br>Extending – overview of the plug in architecture and high-level intro to creating modules<br>Ti+ - look into the modules available and how you could use them in your app<br><br>Office Hours – time for looking into your app, your questions",
+		"%l_slidenote_roadmap": "We'll start by sharing techniques we've learned for creating performant apps<br>Good UX separates average from best of breed apps<br>Advanced UI digs into creating custom UI components<br>TableView &mdash; dig deep into creating custom tables and rows<br>Gestures &mdash; handle orientation, shakes, and other gestures<br>Input &mdash; mobile devices not optimal for data entry, so we'll show some tricks and techniques<br>ScrollView &mdash; ScrollView and ScrollableView components, expose functionality over time rather than jamming all your UI into the viewport<br>Animation &mdash; important from a UX standpoint, can add eye-candy to your app also<br>Android API &mdash; dig into some android specific features you can implement<br>iOS API &mdash; dig into iOS specific UI components, add to settings screen<br>Extending &mdash; overview of the plug in architecture and high-level intro to creating modules<br>Ti+ - look into the modules available and how you could use them in your app<br><br>Office Hours &mdash; time for looking into your app, your questions",
+
 		"%l_func_fab": "Functional to Fabulous",
-		"%l_slidenote_func_fab": "Our first order of business is to take TiBountyHunter and convert it from a novice level application to a polished, professional quality application.<br>Implement a custom tab bar, cross platform<br>Custom windowing, better visual assets",
+		"%l_slidenote_func_fab": "We won't be working with TiBountyHunter is this course. But these screens still illustrate our goal to help you take your apps from functional to fabulous. Learning how to create 'Best of Breed' apps is our goal.",
+
 		"%l_qa": "Questions",
 		"%l_slidenote_qa": ""
 	}
