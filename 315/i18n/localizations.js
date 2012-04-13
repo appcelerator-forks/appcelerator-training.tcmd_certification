430066b75f94961150fb5daef99bbdd3df3a1eec
Tim Poulsen <tim@skypanther.com>
2012-04-13 16:39:44 -0400

    Lesson 315 updated

--- 315/i18n/localizations.js
+++ 315/i18n/localizations.js
@@ -8,37 +8,45 @@ String.toLocaleString({
 		"%l_filetitle": "Advanced UI Development",
 		"%l_subtitle": "Advanced Titanium Mobile Development",
 		"%l_lessontitle": "Advanced UI Development",
-		"%l_slidenote_titleslide": "<b>Module time: 60 mins</b>(30 mins teaching, 30 mins for lab)",
+		"%l_slidenote_titleslide": "<b>Module time: 60 mins</b><br/>(30 mins teaching, 30 mins for lab)",
+
 		"%l_agenda_title": "Agenda",
 		"%l_agenda1": "Building on a good app",
 		"%l_agenda2": "Developing custom navigation",
 		"%l_agenda3": "Getting the most out of your components",
-		"%l_slidenote_agenda": "Take your app from standard to exceptional<br>Quick overview of building a good app. More and more apps are adopting a custom tab bar as part of their application. This is a trend that is constantly going to evolve, but provides a conundrum to Titanium users that don’t have custom module experience. How to I replicate that experience?<br>While this largely focuses on how to develop a custom tab bar, largely this is about brand and using components that are not normally used for improved design.<br>We’ll discuss custom navigation and how to get the most out of your components. Since most of you know how to style and improve upon the existing components, I would encourage you to think how those can be used in different ways to vary the user interface to help drive the experience of your consumer.",
+		"%l_slidenote_agenda": "In the last lesson, we talked UX -- app flow, ease of use, etc.<br/>In this lesson, we're talking UI -- how your app looks. We'll also look at how you can incorporate your brand into your design.",
+
 		"%l_std_exc": "Standard to Exceptional",
+		"%l_l_exceptional_image": "<img src='images/nbc.png'>",
 		"%l_slidenote_std_exc": "",
+
 		"%l_brand_docs": "Brand Documents",
 		"%l_understand_customer": "Understanding Your Consumer",
 		"%l_develop_single": "Develop a Single Page Brand",
-		"%l_know_the": "Know the Emotion, Style & Location",
+		"%l_know_the": "Know the Emotion, Style &amp; Location",
 		"%l_slidenote_brand_docs": "Incorporating brand is critical<br>Develop a brand document which describes<br><ul><li>Strategy</li><li>consumer needs</li><li>Standard colors, fonts, etc.</li></ul><br>If you do not have a brand document, then this should be one of your first objectives. There are several ways to go about getting one, and developing one, but the fact is that you should at the least have a cheat sheet describing your strategy, consumer needs, and a list of your primary and secondary colors, fonts and icons if developed.<br> - These should revolve around understanding your consumer, not yourselves! If you understand your consumers trends, then it will help dictate the design elements<br> - Develop a single page brand that everyone can adopt. This will help tremendously so that there is no lack of consistency in design and implementation.<br>Lastly understand the emotion of your company, the culture behind it, locate the style of how you represent the brand and where your targets are. This will help develop artistic style and guide in the principles of what the app should convey to the user.",
+
 		"%l_inc_brand": "Incorporation of Brand",
 		"%l_app_icons": "App Icons & Default Screens",
-		"%l_colors_ux": "Colors & UX",
+		"%l_colors_ux": "Colors &amp; UX",
 		"%l_ui_comps": "UI Components",
-		"%l_slidenote_inc_brand": "How can you incorporate your brand?<ul><li>Obviously logos, icons, images</li><li>Colors should flow through UI</li><li>Custom components</li></ul>Brand incorporation needs to start at some of the most fundamental levels. The app icon and default screens are the easiest ones to attack. Once you understand the brand, focus the UI on the core message to the consumer. Your app icon is the very first piece of art that people will judge your app on. Design it wisely and make sure that it flows with your app appropriately.<br>As mentioned in the last module, make sure that your brand colors flow in the appropriate areas through your application. If you determine all headers are to be white, make sure that you are consistent. This is deciding brand rules for your application design. Designers often don’t think of apps as interactive pieces all the time so if you see variances in the artwork make sure to ask “why”. It is very important for the designer to describe every pixel on the screen.<br>UI components are also very easy to modify if you have the assets available. Make you that you use consistent colors and designs throughout your application that accurately represent what they are.",
+		"%l_slidenote_inc_brand": "How can you incorporate your brand?<ul><li>Obviously logos, icons, images</li><li>Colors should flow through UI</li><li>Custom components</li></ul>Brand incorporation needs to start at some of the most fundamental levels. The app icon and default screens are the easiest ones to attack. Once you understand the brand, focus the UI on the core message to the consumer. Your app icon is the very first piece of art that people will judge your app on. Design it wisely and make sure that it flows with your app appropriately.<br>As mentioned in the last module, make sure that your brand colors flow in the appropriate areas through your application. If you determine all headers are to be white, make sure that you are consistent. This is deciding brand rules for your application design. Designers often don't think of apps as interactive pieces all the time so if you see variances in the artwork make sure to ask “why”. It is very important for the designer to describe every pixel on the screen.<br>UI components are also very easy to modify if you have the assets available. Make you that you use consistent colors and designs throughout your application that accurately represent what they are.",
+
 		"%l_custom_fonts": "Custom Fonts",
 		"%l_one_of": "One of the quickest and easiest items",
 		"%l_dont_over": "Don't over use",
 		"%l_use_where": "Use where appropriate",
 		"%l_adhere_to": "Adhere to brand",
-		"%l_slidenote_custom_fonts": "Custom fonts are one of the easiest items to improve upon in any application. Make sure that you cross reference the list of available fonts on device before actually including them in your app. There is no reason to have unnecessary overhead. Adding custom fonts will also allow you to identify areas where images are not necessarily needed.<br><ul><li>Ensure that you don’t overuse custom fonts in the application. Script fonts are nice, but not everywhere. Use them in key areas of your application that need the added brand message.</li><li>This means using them where appropriate in your application. If this isn’t defined in your brand document, write down where and how your using the custom fonts. The more you define the details of usage the better.</li><li>This means adhering to your brand. The more you define the details of your brand, the better.</li></ul>",
+		"%l_slidenote_custom_fonts": "Custom fonts are easy way to incorporate brand.<br/>Don't make your app look like a ransom note<br/>Fonts can replace images -- symbol &amp; dingbat style fonts<br/>Great free fonts at fontsquirrel.com and Google fonts",
+
 		"%l_cust_nav": "Custom Navigation",
-		"%l_slidenote_cust_nav": "With that being said, let’s dive into a custom navigation example and how we can improve upon the apps infrastructure and UI.",
+		"%l_slidenote_cust_nav": "With that being said, let's dive into a custom navigation example and how we can improve upon the apps infrastructure and UI.",
 		"%l_commonality": "Commonality",
 		"%l_common_comps": "Common Components are Boring",
 		"%l_what_is": "What is not Branded",
-		"%l_title_tab": "Title & Tab",
-		"%l_slidenote_commonality": "Simply put common components are very boring. <br>Make sure to also consider what’s often not branded including title and tab bars<br>A simple tab bar looks the same in every application. Same goes for the default title control. Sure you can change color, but believe it or now, most don’t spend the time to do so.<br>This leads us to what is not by default branded in your application and this is the tab bar and title control. By owing these two items that are consistent across most of your app, you can drastically change the appeal, emotion, and style of your final application.<br>We’ll focus specifically on the title and tab bar.",
+		"%l_title_tab": "Title &amp; Tab",
+		"%l_slidenote_commonality": "Simply put common components are very boring. <br>Make sure to also consider what's often not branded including title and tab bars<br>A simple tab bar looks the same in every application. Same goes for the default title control. Sure you can change color, but believe it or now, most don't spend the time to do so.<br>This leads us to what is not by default branded in your application and this is the tab bar and title control. By owing these two items that are consistent across most of your app, you can drastically change the appeal, emotion, and style of your final application.<br>We'll focus specifically on the title and tab bar.",
+
 		"%l_custom_comps": "Creating Custom Components",
 		"%l_build_blocks": "Building Blocks: Windows, Views, ImageViews",
 		"%l_pros": "Pros: Cross-platform, strongly branded",
@@ -46,7 +54,8 @@ String.toLocaleString({
 		"%l_nav_comps": "Navigation components (tab navigators) are extra work",
 		"%l_try_to": "Try to generalize them as much as possible and DESIGN THE API",
 		"%l_demo": "Demo",
-		"%l_slidenote_custom_comps": "Views & imageViews are the primary means to create custom UI components <br>Use background images, for example<br>With custom navigation components, you lose A LOT of built-in behavior – sliding behavior, back button behavior, currentTab/currentWindow, etc.<br><b>Demo: see Mod315Demo project in Studio</b>",
+		"%l_slidenote_custom_comps": "Views &amp; imageViews are the primary means to create custom UI components <br>Use background images, for example<br>With custom navigation components, you lose A LOT of built-in behavior &mdash; sliding behavior, back button behavior, currentTab/currentWindow, etc.<br><b>Demo: see Mod315Demo project in GitHub repo</b>",
+
 		"%l_qa": "Q&amp;A",
 		"%l_slidenote_qa": "",
 		"%l_lab": "Lab Goals",
@@ -56,8 +65,14 @@ String.toLocaleString({
 		"%l_cust_hdr": "custom header",
 		"%l_demo_wiki_url": "Demo and wiki URL",
 		"%l_ios_only": "<i>* This lab is iOS only</i>",
-		"%l_slidenote_lab_goals": "In this lab, you will create a custom tab bar component. It will display two tabs and feature a sliding tab indicator with a custom background image.<br>Techniques used in this lab won’t work on Android",
+		"%l_slidenote_lab_goals": "In this lab, you will create a custom tab bar component. It will display two tabs and feature a sliding tab indicator with a custom background image.<br>Techniques used in this lab won't work on Android",
+
 		"%l_lab": "Lab",
-		"%l_slidenote_lab": "Walk through the finished code<br>Can also walk through the finished TiBH custom component code<br>Code walkthrough high points – ui.js, the tabView component function tabView())<br>The custom tab bar is the primary bit of custom/advanced UI in the app.  Walk through how this component works, how it uses custom events, and how we add functions to the object, like:<br><pre>container.toggle = function(on) {icon.image = (on) ? options.on : options.off;container.backgroundImage = (on) ? 'images/tab.png' : '';};</pre>"
+		"%l_slidenote_lab": "",
+
+		"%l_solutionwalkthrough": "Solution Walkthrough",
+		"%l_slidenote_solutionwalkthrough": "Explain <code>createCustomTab()</code> &mdash; extends a standard tab group<br/><code>makeTab()</code> &mdash; extends standard tabs with graphics and labels<br/>Calls to <code>makeTab()</code> on ~line 85 to instantiate the tabs<br/>Show tab bar background image to illustrate how the sliding effect is created<br/>Set <code>window.bottom=200</code> to reveal the 'magic' of this trick",
+
+
 	}
 });
