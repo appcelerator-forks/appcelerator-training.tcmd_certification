499570eaac187ea8855b8cca08766a0f6d3d81d5
Gaurav Kheterpal <gauravkheterpal@yahoo.com>
2012-04-16 12:32:42 +0530

    Conversion for Chapter 375

---
+++ 375/i18n/localizations.js
@@ -0,0 +1,114 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_filetitle": "Titanium Plus Integration",
+		"%l_subtitle": "Advanced Titanium Mobile Development",
+		"%l_lessontitle": "Titanium Plus Integration",
+		"%l_slidenote_titleslide": "<b>Module time: 30 mins</b>(10 mins teaching, 20 mins for lab)",
+
+		"%l_agenda_title": "Agenda",
+		"%l_agenda1": "Titanium Plus Module Overview",
+		"%l_agenda2": "Module Install",
+		"%l_agenda3": "Lab - Exploring Plus Modules",
+		"%l_slidenote_agenda": "<b>This is basically an overview of being a Ti+ consumer.</b>",
+
+		"%l_what_ti_plus": "What is Titanium Plus",
+		"%l_ti_plus_image": "<img src='images/ti-plus.png' style='margin-top:5px;height:300px;float:right;'/>",
+		"%l_val_add": "Value add modules",
+
+		"%l_narrow_use": "Narrow use cases",
+		"%l_appc_created": "Appcelerator created",
+		"%l_comm_created": "Community created",
+		"%l_slidenote_ti_plus": "titanium plus is a constellation of appc and user created modules<br>core functionality that is horizontally useful to devs is likely to go into core<br>specific use cases like barcode scanning, credit card reading, etc. is likely to move into modules",
+
+		"%l_plus_access": "Titanium Plus Access",
+		"%l_all_subs": "Titanium Plus available to all subscribers",
+		"%l_distr_thru": "Distributed through Marketplace",
+		"%l_in_future": "In the future, might be done through tooling",
+		"%l_slidenote_plus_access": "",
+
+		"%l_mod_overview": "Module Overview",
+		"%l_slidenote_mod_overview": "",
+		
+		"%l_urban_airship": "Urban Airship (iOS and Android)",
+		"%l_ti_urban_image": "<img src='images/urban-airship.png' style='margin-top:5px;height:300px;float:right;'/>",
+		"%l_push_not": "Push notifications service",
+		"%l_client_side": "Titanium deals with the client side only",
+		"%l_server_side": "Urban Airship provides help for the server side, but much of that work is up to you",
+		"%l_slidenote_urban_airship": "",
+		
+		"%l_paypal": "PayPal (iOS and Android)",
+		"%l_paypal_image": "<img src='images/paypal.png' style='margin-top:5px;height:200px;float:right;'/>",
+		"%l_mob_payments": "Mobile Payments Library",
+		"%l_merchant": "Requires PayPal merchant account",
+		"%l_dev": "In development, uses sandbox environment, in production uses live servers",
+		"%l_slidenote_paypal": "Useful for physical purchases or donations<br>Can’t be used for ebooks, unlocking app features, etc. because those uses would run afoul of Apple’s StoreKit rules",
+
+		"%l_bump": "Bump",
+		"%l_bump_image": "<img src='images/bump.png' style='margin-top:5px;height:350px;float:right;'/>",
+		"%l_dev_to_dev": "Device to device communication",
+		"%l_wifi": "Works over wifi",
+		"%l_geoloc": "Uses geolocation and requires close proximity",
+		"%l_bump_api": "Requires Bump API key set",
+		"%l_slidenote_bump": "Have to register for an API key with Bump",
+		
+		"%l_sms": "SMS (iOS)",
+		"%l_sms_image": "<img src='images/sms.png' style='margin-top:5px;height:200px;float:right;'/>",
+		"%l_prep_sms": "Prepare SMS text messages in app",
+		"%l_code": "Cannot send via code",
+		"%l_enhanced": "Enhanced functionality over launching the SMS app",
+		"%l_slidenote_sms": "",
+		
+		"%l_tv_out": "TV Out (iOS)",
+		"%l_tv_out_image": "<img src='images/tv-out.png' style='margin-top:5px;height:350px;'/>",
+		"%l_display_app": "Display app contents on TV screen with additional cable",
+		"%l_test_scaling": "Test scaling on TV screen, will vary in quality",
+		"%l_slidenote_tv_out": "",
+		
+		"%l_storekit": "StoreKit (iOS)",
+		"%l_storekit_image": "<img src='images/storekit.png' style='margin-top:5px;height:200px;float:right;'/>",
+		"%l_in_app": "In App Purchase API",
+		"%l_itunes_conn": "Requires set up through iTunes Connect portal",
+		"%l_on_dev": "Can only be tested on device",
+		"%l_apple_docs": "Will need to follow Apple docs for user and item creation",
+		"%l_slidenote_storekit": "Tied to your iTunes account which is why you have to test on device (that linkage isn’t available in the simulator)",
+		
+		"%l_magtek": "Magtek Credit Card Reader (iOS)",
+		"%l_magtek_image": "<img src='images/magtek.png' style='margin-top:5px;height:200px;float:right;'/>",
+		"%l_interface": "Interface to Magtek hardware",
+		"%l_can_process": "Can process card payments",
+		"%l_add_acc": "Requires additional account setup for payments",
+		"%l_peripheral": "Can only be tested on device with peripheral",
+		"%l_slidenote_magtek": "",
+		
+		"%l_more_come": "More To Come!",
+		"%l_slidenote_more_come": "Appc has a dedicated module development team now, so this offering will continue to grow",
+		
+		"%l_install_mod": "Installing Modules",
+		"%l_unzip": "Download and Unzip from Helpdesk",
+		"%l_copy_into": "Copy into modules directory in project root",
+		"%l_declare": "Declare module dependency in tiapp.xml",
+		"%l_require": "require the module in JavaScript",
+		"%l_use_mod": "Use module - every one comes with example and doc",
+		"%l_demo_install": "Demo: Install and run Ti+ Module",
+		"%l_slidenote_install_mod": "PayPal works well for demo since you can do it cross platform and it works on Simulator",
+
+		"%l_qa": "Q&amp;A",
+		"%l_slidenote_qa": "",
+		
+		"%l_lab_goals": "Lab Goals",
+		"%l_download_install": "Download and install the PayPal module",
+		"%l_include_app": "Include it within an app",
+		"%l_enable_sandbox": "Enable sandbox-mode PayPal functionality in your app",
+		"%l_wiki_url": "Wiki URL",
+		"%l_slidenote_lab_goals": "Will need to be able to install and demo a module, any module.  PayPal works well for demo since you can do it cross platform and it works on Simulator",
+		
+		"%l_lab": "Lab",
+		"%l_slidenote_lab": "no TiBountyHunter plug in here – going through the PayPal module."
+
+	}
+});
