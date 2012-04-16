9f96a20573c6672e6487f8f1fe524c2121e6da72
Gaurav Kheterpal <gauravkheterpal@yahoo.com>
2012-04-17 03:05:42 +0530

    Conversion for Chapter 360

---
+++ 360/i18n/localizations.js
@@ -0,0 +1,34 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_filetitle": "iOS Deep Dive",
+		"%l_subtitle": "Advanced Titanium Mobile Development",
+		"%l_lessontitle": "iOS Deep Dive",
+		"%l_slidenote_titleslide": "<b>Module time: 90 mins</b>(60 mins teaching, 30 mins for lab)",
+
+		"%l_agenda_title": "Agenda",
+		"%l_agenda1": "iOS Platform Characteristics",
+		"%l_agenda2": "iOS-specific API Overview",
+		"%l_agenda3": "Key APIs",
+		"%l_agenda4": "Deep Dive: iOS Properties",
+		"%l_agenda5": "Examples in Action",
+		"%l_agenda6": "Lab Exercise Overview",
+		"%l_slidenote_agenda": "We will give an overview on the platform characteristics, including basic UI structure.  Then we’ll focus on iOS-specific APIs, starting with a high level view of what the key areas are in Titanium.Let the deep dive begin..",
+
+		"%l_qa": "Q&amp;A",
+		"%l_slidenote_qa": "",
+		
+		"%l_lab_goals": "Lab Goals",
+		"%l_add_settings": "Add settings to an iOS application",
+		"%l_demo_wiki": "Demo and wiki location",
+		"%l_slidenote_lab_goals": "In this lab, you will create a Settings bundle so that application settings can be changed via the Settings app in the simulator or on a phone. You will then use that setting within the app to control the app's user interface",
+		
+		"%l_lab": "Lab",
+		"%l_slidenote_lab": "There is no specific TiBountyHunter tie-in for this lab"
+
+	}
+});
