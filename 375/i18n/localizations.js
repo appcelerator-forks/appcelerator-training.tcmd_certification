dddabafac57aac3dbe1b7dfaf7abf69c162f96fa
Tim Poulsen <tim@skypanther.com>
2012-05-04 17:11:59 -0400

    HTML updates, new slides

--- 375/i18n/localizations.js
+++ 375/i18n/localizations.js
@@ -5,34 +5,33 @@
 */
 String.toLocaleString({
 	"en-US": {
-		"%l_filetitle": "Titanium Plus Integration",
+		"%l_filetitle": "Using Modules in your Apps",
 		"%l_subtitle": "Advanced Titanium Mobile Development",
-		"%l_lessontitle": "Titanium Plus Integration",
-		"%l_slidenote_titleslide": "<b>Module time: 30 mins</b>(10 mins teaching, 20 mins for lab)",
+		"%l_lessontitle": "Using Modules in your Apps",
+		"%l_slidenote_titleslide": "<b>Module time: 30 mins</b><br/>(15 mins teaching, 20 mins for lab)",
 
 		"%l_agenda_title": "Agenda",
-		"%l_agenda1": "Titanium Plus Module Overview",
+		"%l_agenda1": "Titanium Modules Overview",
 		"%l_agenda2": "Module Install",
-		"%l_agenda3": "Lab - Exploring Plus Modules",
-		"%l_slidenote_agenda": "<b>This is basically an overview of being a Ti+ consumer.</b>",
+		"%l_agenda3": "Lab &mdash; Adding a Module to a Project",
+		"%l_slidenote_agenda": "You'll explore a few of the free modules available to community-level developers, as well as cover how to add a module to your project.",
 
-		"%l_what_ti_plus": "What is Titanium Plus",
+		"%l_what_ti_plus": "Titanium Modules",
 		"%l_ti_plus_image": "<img src='images/ti-plus.png' style='margin-top:5px;height:300px;float:right;'/>",
-		"%l_val_add": "Value add modules",
-
+		"%l_val_add": "Add 'missing' features",
 		"%l_narrow_use": "Narrow use cases",
-		"%l_appc_created": "Appcelerator created",
-		"%l_comm_created": "Community created",
-		"%l_slidenote_ti_plus": "titanium plus is a constellation of appc and user created modules<br>core functionality that is horizontally useful to devs is likely to go into core<br>specific use cases like barcode scanning, credit card reading, etc. is likely to move into modules",
+		"%l_created": "Appcelerator &amp; Community created",
+		"%l_slidenote_ti_plus": "<ul><li>Add features to Titanium not included in the core distribution</li><li>Typically narrowly focused on adding a specific feature, rather than a broad API addition</li><li>Some written by Appcelerator, others by the community. Available in a wide range of prices</li></ul>",
 
-		"%l_plus_access": "Titanium Plus Access",
-		"%l_all_subs": "Titanium Plus available to all subscribers",
+		"%l_plus_access": "Module Distribution",
+		"%l_licenses": "Variety of licensing options",
 		"%l_distr_thru": "Distributed through Marketplace",
-		"%l_in_future": "In the future, might be done through tooling",
-		"%l_slidenote_plus_access": "",
+		"%l_helpdesk": "'Official' Modules Distributed through Helpdesk",
+		"%l_otherdistro": "Also, GitHub, Blogs, etc.",
+		"%l_slidenote_plus_access": "Variety of distribution methods are used. Modules can be sold or distributed via marketplace.appcelerator.com. Open-source and unofficial modules are often distributed by their authors via GitHub, blogs, and so forth.",
 
-		"%l_mod_overview": "Module Overview",
-		"%l_slidenote_mod_overview": "",
+		"%l_mod_overview": "Module Quick-Tour",
+		"%l_slidenote_mod_overview": "A quick look at some of the available modules",
 		
 		"%l_urban_airship": "Urban Airship (iOS and Android)",
 		"%l_ti_urban_image": "<img src='images/urban-airship.png' style='margin-top:5px;height:300px;float:right;'/>",
@@ -85,17 +84,32 @@ String.toLocaleString({
 		"%l_peripheral": "Can only be tested on device with peripheral",
 		"%l_slidenote_magtek": "",
 		
-		"%l_more_come": "More To Come!",
+		"%l_more_come": "Many More!",
+		"%l_more_img": "<img src='images/marketplace.png' style='width:550px;'/>",
 		"%l_slidenote_more_come": "Appc has a dedicated module development team now, so this offering will continue to grow",
 		
 		"%l_install_mod": "Installing Modules",
-		"%l_unzip": "Download and Unzip from Helpdesk",
-		"%l_copy_into": "Copy into modules directory in project root",
-		"%l_declare": "Declare module dependency in tiapp.xml",
-		"%l_require": "require the module in JavaScript",
-		"%l_use_mod": "Use module - every one comes with example and doc",
-		"%l_demo_install": "Demo: Install and run Ti+ Module",
-		"%l_slidenote_install_mod": "PayPal works well for demo since you can do it cross platform and it works on Simulator",
+		"%l_install_options": "Two install options: for single project or all projects",
+		"%l_install_single": "Install for single project:",
+		"%l_install1": "Download zip file",
+		"%l_install2": "Place in project's root directory",
+		"%l_install3": "Build once &mdash; to unzip and create necessary folders",
+		"%l_install4": "Add to tiapp.xml and JavaScript",
+		"%l_slidenote_install_mod": "Installing for all projects doesn't add overhead to your projects, so is probably the best option. We'll cover how to update tiapp.xml and JavaScript on an upcoming slide.",
+
+		"%l_install_all": "Installing Modules, Continued",
+		"%l_installall": "Install for all projects:",
+		"%l_installall1": "Download zip file",
+		"%l_installall2": "Unzip to %TITANIUM_INSTALL_DIR%/modules directory",
+		"%l_installall3": "Add to tiapp.xml and JavaScript",
+		"%l_slidenote_installall": "Any future projects you develop on this computer would then be able to use the module.<br/><br/>The Titanium install directory varies by platform and operating system version. For example, on OS X Lion, it would be ~/Library/Application Support/Titanium.",
+
+		"%l_implementmodule": "Load and Require Module",
+		"%l_implement1": "<img src='images/studio_modules.png' style='width:400px;float:right;'/>Declare in tiapp.xml",
+		"%l_implement2": "<img src='images/module_require.png' style='clear:right;width:300px;float:right;'/>Require in JavaScript",
+		"%l_implement3": "Implement",
+		"%l_use_mod": "(Every marketplace module comes with example and doc)",
+		"%l_slidenote_implementmodule": "",
 
 		"%l_qa": "Q&amp;A",
 		"%l_slidenote_qa": "",
@@ -104,11 +118,7 @@ String.toLocaleString({
 		"%l_download_install": "Download and install the PayPal module",
 		"%l_include_app": "Include it within an app",
 		"%l_enable_sandbox": "Enable sandbox-mode PayPal functionality in your app",
-		"%l_wiki_url": "Wiki URL",
-		"%l_slidenote_lab_goals": "Will need to be able to install and demo a module, any module.  PayPal works well for demo since you can do it cross platform and it works on Simulator",
-		
-		"%l_lab": "Lab",
-		"%l_slidenote_lab": "no TiBountyHunter plug in here – going through the PayPal module."
-
+		"%l_labURL": "<a href='https://wiki.appcelerator.org/display/td/370+-+Installing+and+using+a+Titanium+Module'>wiki.appcelerator.org/display/td/370+-+Installing+and+using+a+Titanium+Module</a>",
+		"%l_slidenote_lab_goals": "Will need to be able to install and demo a module, any module.  PayPal works well for demo since you can do it cross platform and it works on Simulator"
 	}
 });
